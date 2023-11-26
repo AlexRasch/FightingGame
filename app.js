ed829ed93b4753eec567700e859dafb9f9889298
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-11-26 17:45:24 +0100

    spawn player and enemy working

--- app.js
+++ app.js
@@ -6,32 +6,64 @@ canvas.height = 576;
 
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-
-
 // Todo move to a new file.js :>
+const gravity = 0.2;
 class Sprite {
-    constructor(position) {
+    constructor({position, velocity}) {
         this.position = position;
+        this.velocity = velocity;
+        this.height = 150;
     }
 
     draw(){
         canvasContext.fillStyle = "red";
-        canvasContext.fillRect(this.position.x, this.position.y, 50 , 150);
+        canvasContext.fillRect(this.position.x, this.position.y, 50 , this.height);
+    }
+
+    update(){
+        this.draw();
+        this.position.y += this.velocity.y
+
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        }else{
+            this.velocity.y += gravity;
+        }
     }
 }
 
 // Create player & enemy
 const player = new Sprite({
-    x: 50,
-    y: 400
+    position:{
+        x: 50,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 });
 
 const enemy = new Sprite({
-    x: 850,
-    y: 400
+    position:{
+        x: 850,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 });
 
-player.draw();
-enemy.draw();
 
-console.log(player);
+function animate() {
+    window.requestAnimationFrame(animate);
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    
+    player.update();
+    enemy.update();
+    
+}
+
+animate()
